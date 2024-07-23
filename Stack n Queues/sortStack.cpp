ae4e46fc868ce58cc7009e191871355c4d13a329
Vivek Choudhary <149962317+VivekChoudhary15@users.noreply.github.com>
2024-07-23 21:05:56 +0530

    Update sortStack.cpp

--- Stack n Queues/sortStack.cpp
+++ Stack n Queues/sortStack.cpp
@@ -1,3 +1,5 @@
+// P.S.: https://www.naukri.com/code360/problems/sort-a-stack_985275?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
 #include<iostream>
 #include<stack>
 using namespace std;
@@ -27,4 +29,32 @@ void sortStack(stack<int> &Stack){
     sortStack(Stack);
 
     insertInSortedOrder(Stack, topElement);
+}
+
+// Utility function to print the stack
+void printStack(std::stack<int> st) {
+    while (!st.empty()) {
+        std::cout << st.top() << " ";
+        st.pop();
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::stack<int> st;
+    st.push(30);
+    st.push(20);
+    st.push(50);
+    st.push(10);
+    st.push(40);
+
+    std::cout << "Original stack: ";
+    printStack(st);
+
+    sortStack(st);
+
+    std::cout << "Sorted stack (descending order): ";
+    printStack(st);
+
+    return 0;
 }
